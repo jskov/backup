62537e73a6e4160d6ec7f8763844a49cf1d9d3b4
Jesper Skov <jskov@zoftcorp.dk>
2019-07-20 11:30:11 +0200

    Reorg restore execution

--- src/main/java/dk/mada/backup/cli/Main.java
+++ src/main/java/dk/mada/backup/cli/Main.java
@@ -1,16 +1,13 @@
 package dk.mada.backup.cli;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 import com.beust.jcommander.JCommander;
 
 import dk.mada.backup.BackupApi;
+import dk.mada.backup.restore.RestoreExecutor;
 
 /**
  * Main method for CLI invocation.
@@ -38,7 +35,6 @@ public class Main {
 	}
 
 	private void run() {
-		
 		BackupApi backupApi = new BackupApi(cliArgs.getGpgRecipientId(), envOverrides);
 		Path restoreScript = backupApi.makeBackup(cliArgs.getBackupName(), cliArgs.getSourceDir(), cliArgs.getTargetDir());
 		
@@ -52,40 +48,14 @@ public class Main {
 	private void verifyBackup(Path script) {
 		try {
 			System.out.println("Verifying backup...");
-			runRestoreCmd(script, "verify");
-			runRestoreCmd(script, "verify", "-s");
+			RestoreExecutor.runRestoreScriptExitOnFail(script, envOverrides, "verify");
+			RestoreExecutor.runRestoreScriptExitOnFail(script, envOverrides, "verify", "-s");
 			System.out.println("Backup verified.");
 		} catch (Exception e) {
 			throw new IllegalStateException("Failed to run verify script " + script, e);
 		}
 	}
-	
-	private void runRestoreCmd(Path script, String... args) throws IOException, InterruptedException {
-		List<String> cmd = new ArrayList<>(List.of("/bin/bash", script.toAbsolutePath().toString()));
-		cmd.addAll(List.of(args));
-		ProcessBuilder pb = new ProcessBuilder(cmd)
-				.directory(script.getParent().toFile())
-				.redirectErrorStream(true);
-		
-		pb.environment().putAll(envOverrides);
-		
-		Process p = pb.start();
-		
-		String output = readOutput(p);
-		
-		if (p.waitFor() != 0) {
-			System.out.println("Failed to verify backup");
-			System.out.println(output);
-			System.exit(1);
-		}
-	}
 
-	private String readOutput(Process p) throws IOException {
-         try (InputStream in = p.getInputStream()) {
-             return new String(in.readAllBytes());
-         }
-	}
-	
 	public static void main(String[] args) {
 		new Main(args).run();
 	}
