ee12c69f7fa89a4a370bf08d454589c7cdd125ac
Jesper Skov <jskov@zoftcorp.dk>
2019-11-22 12:40:47 +0100

    Update docker image for Actions

--- src/test/java/fixture/TestCertificateInfo.java
+++ src/test/java/fixture/TestCertificateInfo.java
@@ -10,16 +10,16 @@ import java.util.Map;
  * test certificate in src/test/data/gpghome.
  */
 public class TestCertificateInfo {
-	private static final Path IMPORT_TEST_GNUPG_HOME = Paths.get("build/azure-gpg-home");
+	private static final Path IMPORT_TEST_GNUPG_HOME = Paths.get("build/actions-gpg-home");
 	private static final Path LOCAL_SOURCE_TEST_GNUPG_HOME = Paths.get("src/test/data/gpghome");
-	private static final Path TEST_GNUPG_HOME = (isRunningOnAzure() ? IMPORT_TEST_GNUPG_HOME : LOCAL_SOURCE_TEST_GNUPG_HOME);
+	private static final Path TEST_GNUPG_HOME = (isRunningOnGitHubActions() ? IMPORT_TEST_GNUPG_HOME : LOCAL_SOURCE_TEST_GNUPG_HOME);
 	public static final String ABS_TEST_GNUPG_HOME = TEST_GNUPG_HOME.toAbsolutePath().toString();
 	public static final String TEST_RECIPIEND_KEY_ID = "281DE650E39B5DCA3E9D542092B7BAA1D6B4A52D";
 	public static final Map<String, String> TEST_KEY_ENVIRONMENT_OVERRIDES = Map.of("GNUPGHOME", ABS_TEST_GNUPG_HOME);
 
 	private TestCertificateInfo() {}
 	
-	private static boolean isRunningOnAzure() {
+	private static boolean isRunningOnGitHubActions() {
 		return Files.exists(Paths.get("/etc/debian_version"));
 	}
 
