d17351253ea5f0806228ae79990d3e960b989ce4
Jesper Skov <jskov@zoftcorp.dk>
2019-12-07 11:00:25 +0100

    Update human byte count

--- src/main/java/dk/mada/backup/cli/HumanByteCount.java
+++ src/main/java/dk/mada/backup/cli/HumanByteCount.java
@@ -3,14 +3,18 @@ package dk.mada.backup.cli;
 // From https://programming.guide/java/formatting-byte-size-to-human-readable-format.html
 public class HumanByteCount {
 	public static String humanReadableByteCount(long bytes) {
-		return humanReadableByteCount(bytes, false);
-	}
+		return humanReadableByteCountBin(bytes);
 
-	private static String humanReadableByteCount(long bytes, boolean si) {
-	    int unit = si ? 1000 : 1024;
-	    if (bytes < unit) return bytes + " B";
-	    int exp = (int) (Math.log(bytes) / Math.log(unit));
-	    String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
-	    return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+	}
+	
+	public static String humanReadableByteCountBin(long bytes) {
+	    long b = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+	    return b < 1024L ? bytes + " B"
+	            : b <= 0xfffccccccccccccL >> 40 ? String.format("%.1f KiB", bytes / 0x1p10)
+	            : b <= 0xfffccccccccccccL >> 30 ? String.format("%.1f MiB", bytes / 0x1p20)
+	            : b <= 0xfffccccccccccccL >> 20 ? String.format("%.1f GiB", bytes / 0x1p30)
+	            : b <= 0xfffccccccccccccL >> 10 ? String.format("%.1f TiB", bytes / 0x1p40)
+	            : b <= 0xfffccccccccccccL ? String.format("%.1f PiB", (bytes >> 10) / 0x1p40)
+	            : String.format("%.1f EiB", (bytes >> 20) / 0x1p40);
 	}
 }
