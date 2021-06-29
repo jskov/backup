caaca30638f5ac1d868e85f4be10a1464f2479fd
Jesper Skov <jskov@zoftcorp.dk>
2021-06-29 12:36:29 +0200

    update

--- src/test/create-test-key.sh
+++ src/test/create-test-key.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-rm -rf src/test/data/gpghome/openpgp* src/test/data/gpghome/private-keys* src/test/data/gpghome/pubring* src/test/data/gpghome/trust*
+set -e
+
+rm -rf src/test/data/gpghome
+mkdir -p src/test/data/gpghome
+
+#/openpgp* src/test/data/gpghome/private-keys* src/test/data/gpghome/pubring* src/test/data/gpghome/trust*
 keyId=$(gpg --batch --homedir src/test/data/gpghome --passphrase '' --quick-generate-key testKey default default 1y 2>&1 | grep "gpg: key " | sed 's/gpg: key //; s/ .*//;')
 
-sed -i "s/KEYID.*KEYID/KEYID\*\/${keyId}\/\*KEYID/;" src/test/java/dk/mada/fixture/TestCertificateInfo.java
+
+
+sed -i "s/KEYID.*KEYID/KEYID\*\/\"${keyId}\"\/\*KEYID/;" src/test/java/dk/mada/fixture/TestCertificateInfo.java
