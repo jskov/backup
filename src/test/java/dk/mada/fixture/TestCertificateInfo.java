5d37ea5da0594a867541efa44ebb75dfcd880727
Jesper Skov <jskov@zoftcorp.dk>
2021-06-29 12:27:14 +0200

    markers

--- src/test/java/dk/mada/fixture/TestCertificateInfo.java
+++ src/test/java/dk/mada/fixture/TestCertificateInfo.java
@@ -14,7 +14,7 @@ public class TestCertificateInfo {
 	private static final Path LOCAL_SOURCE_TEST_GNUPG_HOME = Paths.get("src/test/data/gpghome");
 	private static final Path TEST_GNUPG_HOME = (isRunningOnGitHubActions() ? IMPORT_TEST_GNUPG_HOME : LOCAL_SOURCE_TEST_GNUPG_HOME);
 	public static final String ABS_TEST_GNUPG_HOME = TEST_GNUPG_HOME.toAbsolutePath().toString();
-	public static final String TEST_RECIPIEND_KEY_ID = "281DE650E39B5DCA3E9D542092B7BAA1D6B4A52D";
+	public static final String TEST_RECIPIEND_KEY_ID = /*KEYID*/"281DE650E39B5DCA3E9D542092B7BAA1D6B4A52D"/*KEYID*/;
 	public static final Map<String, String> TEST_KEY_ENVIRONMENT_OVERRIDES = Map.of("GNUPGHOME", ABS_TEST_GNUPG_HOME);
 
 	private TestCertificateInfo() {}
