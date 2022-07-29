e67245c4c6b0c0a52350d168fa045514dab0e4b9
Jesper Skov <jskov@mada.dk>
2022-07-29 21:57:34 +0200

    more lint

--- src/main/java/dk/mada/backup/BackupElement.java
+++ src/main/java/dk/mada/backup/BackupElement.java
@@ -1,6 +1,10 @@
 package dk.mada.backup;
 
+/**
+ * An element of a backup.
+ */
 @FunctionalInterface
 public interface BackupElement {
+    /** {@return a summary about the element} */
 	String toBackupSummary();
 }
--- src/main/java/dk/mada/backup/gpg/GpgEncrypterException.java
+++ src/main/java/dk/mada/backup/gpg/GpgEncrypterException.java
@@ -2,11 +2,27 @@ package dk.mada.backup.gpg;
 
 import java.io.IOException;
 
+/**
+ * Wraps failures caused by the external GPG process.
+ */
 public class GpgEncrypterException extends IOException {
-	public GpgEncrypterException(String message) {
+	private static final long serialVersionUID = 2738638113041439082L;
+
+    /**
+     * Constructs new exception.
+     *
+     * @param message the exception message
+     */
+    public GpgEncrypterException(String message) {
 		super(message);
 	}
 
+    /**
+     * Constructs new exception.
+     *
+     * @param message the exception message
+     * @param cause the exception cause
+     */
 	public GpgEncrypterException(String message, Exception cause) {
 		super(message, cause);
 	}
