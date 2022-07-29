c2f66aa7ad6ad7529d1157c3a78379f82e6a3c6e
Jesper Skov <jskov@mada.dk>
2022-07-29 21:50:54 +0200

    fix interrupt

--- src/main/java/dk/mada/backup/restore/RestoreExecutor.java
+++ src/main/java/dk/mada/backup/restore/RestoreExecutor.java
@@ -46,8 +46,11 @@ public class RestoreExecutor {
 			String output = readOutput(p);
 			
 			return new Result(p.waitFor(), output);
-		} catch (IOException | InterruptedException e) {
+		} catch (IOException e) {
 			throw new IllegalStateException("Failed to run restore script " + script, e);
+		} catch (InterruptedException e) {
+		    Thread.currentThread().interrupt();
+		    throw new IllegalStateException("Interrupted restore script " + script, e);
 		}
 	}
 
