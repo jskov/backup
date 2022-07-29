cbb77604311ed9c40bd2ec28ba50cbcba8482f0b
Jesper Skov <jskov@mada.dk>
2022-07-29 21:53:50 +0200

    serialization

--- src/main/java/dk/mada/backup/api/BackupTargetExistsException.java
+++ src/main/java/dk/mada/backup/api/BackupTargetExistsException.java
@@ -4,14 +4,32 @@ package dk.mada.backup.api;
  * Thrown if the backup operation needs to write to any existing files.
  */
 public class BackupTargetExistsException extends BackupException {
-	public BackupTargetExistsException(String message) {
+	private static final long serialVersionUID = 6249807297965412874L;
+
+    /**
+     * Constructs new exception.
+     *
+     * @param message the exception message
+     */
+    public BackupTargetExistsException(String message) {
 		super(message);
 	}
 	
+    /**
+     * Constructs new exception.
+     *
+     * @param cause the exception cause
+     */
 	public BackupTargetExistsException(Throwable cause) {
 		super(cause);
 	}
 
+    /**
+     * Constructs new exception.
+     *
+     * @param message the exception message
+     * @param cause the exception cause
+     */
 	public BackupTargetExistsException(String message, Throwable cause) {
 		super(message, cause);
 	}
