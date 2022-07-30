daac91edfea157edf43f413b048fad47a35a3906
Jesper Skov <jskov@mada.dk>
2022-07-30 09:18:18 +0200

    indent

--- src/main/java/dk/mada/backup/FileInfo.java
+++ src/main/java/dk/mada/backup/FileInfo.java
@@ -15,116 +15,115 @@ import java.util.Objects;
  * Captures information about a file.
  */
 public class FileInfo implements BackupElement {
-	private static final int DIGEST_LENGTH_MD5 = 32;
-	private static final int DIGEST_LENGTH_SHA256 = 64;
-	private final String pathName;
-	private final long size;
-	private final String checksum;
-	private final String md5Checksum;
+    private static final int DIGEST_LENGTH_MD5 = 32;
+    private static final int DIGEST_LENGTH_SHA256 = 64;
+    private final String pathName;
+    private final long size;
+    private final String checksum;
+    private final String md5Checksum;
 
-	private FileInfo(String pathName, long size, String checksum, String md5) {
-		this.pathName = pathName;
-		this.size = size;
-		this.checksum = checksum;
-		this.md5Checksum = md5;
-	}
+    private FileInfo(String pathName, long size, String checksum, String md5) {
+        this.pathName = pathName;
+        this.size = size;
+        this.checksum = checksum;
+        this.md5Checksum = md5;
+    }
 
-	public String getPathName() {
-		return pathName;
-	}
+    public String getPathName() {
+        return pathName;
+    }
 
-	public long getSize() {
-		return size;
-	}
+    public long getSize() {
+        return size;
+    }
 
-	public String getChecksum() {
-		return checksum;
-	}
-	
-	public String getMd5() {
-		return Objects.requireNonNull(md5Checksum, "No MD5 sum captured for " + pathName);
-	}
+    public String getChecksum() {
+        return checksum;
+    }
 
-	public static FileInfo of(String pathName, long size, MessageDigest digest) {
-		return new FileInfo(pathName, size, digestToString(digest, DIGEST_LENGTH_SHA256), null);
-	}
+    public String getMd5() {
+        return Objects.requireNonNull(md5Checksum, "No MD5 sum captured for " + pathName);
+    }
 
-	
-	public static FileInfo from(Path rootDir, Path file) {
-		return from(rootDir, file, false);
-	}
+    public static FileInfo of(String pathName, long size, MessageDigest digest) {
+        return new FileInfo(pathName, size, digestToString(digest, DIGEST_LENGTH_SHA256), null);
+    }
 
-	/**
-	 * Generates file info for crypt files.
-	 * These include md5sum.
-	 * 
-	 * @param rootDir
-	 * @param file
-	 * @return
-	 */
-	public static FileInfo fromCryptFile(Path rootDir, Path file) {
-		return from(rootDir, file, true);
-	}
+    public static FileInfo from(Path rootDir, Path file) {
+        return from(rootDir, file, false);
+    }
 
-	private static FileInfo from(Path rootDir, Path file, boolean includeMd5Sum) {
-		byte[] buffer = new byte[8192];
+    /**
+     * Generates file info for crypt files. These include md5sum.
+     *
+     * @param rootDir
+     * @param file
+     * @return
+     */
+    public static FileInfo fromCryptFile(Path rootDir, Path file) {
+        return from(rootDir, file, true);
+    }
 
-		try (InputStream is = Files.newInputStream(file);
-		        BufferedInputStream bis = new BufferedInputStream(is)) {
-		    MessageDigest digestMd5 = MessageDigest.getInstance("MD5"); // NOSONAR - MD5 used by Jottacloud
-			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			long size = Files.size(file);
+    private static FileInfo from(Path rootDir, Path file, boolean includeMd5Sum) {
+        byte[] buffer = new byte[8192];
 
-			int read;
-			while ((read = bis.read(buffer)) > 0) {
-				digest.update(buffer, 0, read);
-				if (includeMd5Sum) {
-					digestMd5.update(buffer, 0, read);
-				}
-			}
-			String checksum = digestToString(digest, DIGEST_LENGTH_SHA256);
+        try (InputStream is = Files.newInputStream(file);
+                BufferedInputStream bis = new BufferedInputStream(is)) {
+            MessageDigest digestMd5 = MessageDigest.getInstance("MD5"); // NOSONAR - MD5 used by Jottacloud
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            long size = Files.size(file);
 
-			String relPath = rootDir.relativize(file).getFileName().toString();
-			
-			return new FileInfo(relPath, size, checksum, includeMd5Sum ? digestToString(digestMd5, DIGEST_LENGTH_MD5) : null);
-		} catch (IOException e) {
-			throw new UncheckedIOException(e);
-		} catch (NoSuchAlgorithmException e) {
-			throw new IllegalStateException("No algo", e);
-		}
-	}
+            int read;
+            while ((read = bis.read(buffer)) > 0) {
+                digest.update(buffer, 0, read);
+                if (includeMd5Sum) {
+                    digestMd5.update(buffer, 0, read);
+                }
+            }
+            String checksum = digestToString(digest, DIGEST_LENGTH_SHA256);
 
-	private static String digestToString(MessageDigest digest, int length) {
-		byte[] hash = digest.digest();
+            String relPath = rootDir.relativize(file).getFileName().toString();
 
-		BigInteger bigInt = new BigInteger(1, hash);
-		StringBuilder sb = new StringBuilder(bigInt.toString(16));
-		while (sb.length() < length) {
-			sb.insert(0, '0');
-		}
-		String checksum = sb.toString();
-		return checksum;
-	}
+            return new FileInfo(relPath, size, checksum,
+                    includeMd5Sum ? digestToString(digestMd5, DIGEST_LENGTH_MD5) : null);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("No algo", e);
+        }
+    }
 
-	@Override
-	public String toString() {
-		return "FileInfo [pathName=" + pathName + ", size=" + size + ", checksum=" + checksum + "]";
-	}
+    private static String digestToString(MessageDigest digest, int length) {
+        byte[] hash = digest.digest();
 
-	@Override
-	public String toBackupSummary() {
-		StringBuilder sb = new StringBuilder();
-		sb.append('"');
-		sb.append(String.format("% 11d", size));
-		sb.append(',');
-		sb.append(checksum);
-		if (md5Checksum != null) {
-			sb.append(",");
-			sb.append(md5Checksum);
-		}
-		sb.append(',');
-		sb.append(pathName);
-		sb.append('"');
-		return sb.toString();
-	}
+        BigInteger bigInt = new BigInteger(1, hash);
+        StringBuilder sb = new StringBuilder(bigInt.toString(16));
+        while (sb.length() < length) {
+            sb.insert(0, '0');
+        }
+        String checksum = sb.toString();
+        return checksum;
+    }
+
+    @Override
+    public String toString() {
+        return "FileInfo [pathName=" + pathName + ", size=" + size + ", checksum=" + checksum + "]";
+    }
+
+    @Override
+    public String toBackupSummary() {
+        StringBuilder sb = new StringBuilder();
+        sb.append('"');
+        sb.append(String.format("% 11d", size));
+        sb.append(',');
+        sb.append(checksum);
+        if (md5Checksum != null) {
+            sb.append(",");
+            sb.append(md5Checksum);
+        }
+        sb.append(',');
+        sb.append(pathName);
+        sb.append('"');
+        return sb.toString();
+    }
 }
