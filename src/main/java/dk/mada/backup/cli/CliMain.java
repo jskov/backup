070cea220fc558d3995024571185703281c58bd9
Jesper Skov <jskov@mada.dk>
2022-07-31 12:31:57 +0200

    location opts

--- src/main/java/dk/mada/backup/cli/CliMain.java
+++ src/main/java/dk/mada/backup/cli/CliMain.java
@@ -2,7 +2,6 @@ package dk.mada.backup.cli;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
@@ -40,19 +39,16 @@ public final class CliMain implements Callable<Integer> {
     /** The picoCli spec. */
     @Spec private CommandSpec spec;
 
-    @Parameters(arity = "2", description = "backup source and target directories (src-dir target-dir)")
-    List<Path> srcTargetDir;
-
     // FIXME: make proper type: validateWith = GpgRecipientValidator.class
-    @Option(names = OPT_RECIPIENT, description = "GPG recipient key id", required = true)
+    @Option(names = OPT_RECIPIENT, description = "GPG recipient key id", required = true, paramLabel = "ID")
     private String gpgRecipientId;
-    @Option(names = { "-n", "--name" }, description = "backup name (default to source folder name)")
+    @Option(names = { "-n", "--name" }, description = "backup name (default to source folder name)", paramLabel = "NAME")
     private String backupName;
-    @Option(names = "--gpg-homedir", description = "alternative GPG home dir")
+    @Option(names = "--gpg-homedir", description = "alternative GPG home dir", paramLabel = "DIR")
     private Path gpgHomeDir;
     @Option(names = "--skip-verify", description = "skip verification after creating backup")
     private boolean skipVerify;
-    @Option(names = OPT_MAX_SIZE, description = "max file size", converter = HumanSizeInputConverter.class)
+    @Option(names = OPT_MAX_SIZE, description = "max file size", converter = HumanSizeInputConverter.class, paramLabel = "SIZE")
     private long maxFileSize;
     @Option(names = "--running-tests", description = "used for testing to avoid System.exit")
     private boolean testingAvoidSystemExit;
@@ -60,24 +56,22 @@ public final class CliMain implements Callable<Integer> {
     private boolean printVersion;
     @Option(names = "--help", description = "print this help and exit", help = true)
     private boolean printHelp;
-    
+
+    @Parameters(index = "0", description = "backup source directory", paramLabel = "source-dir")
+    private Path sourceDir;
+    @Parameters(index = "1", description = "target directory", paramLabel = "target-dir")
+    private Path targetDir;
+
     /**
      * Creates new instance for a single invocation from CLI.
      *
      * @param args the command line arguments
      */
     public Integer call() {
-//        for (String a : args) {
-//            if ("--version".equals(a)) {
-//                Console.println("Backup version " + Version.getBackupVersion() + " built on " + Version.getBuildTime());
-//                systemExit(0);
-//            }
-//        }
-        
-        if (!Files.isDirectory(getSourceDir())) {
+        if (!Files.isDirectory(sourceDir)) {
             argumentFail("The source directory must be an existing directory!");
         }
-        if (Files.exists(getTargetDir()) && !Files.isDirectory(getTargetDir())) {
+        if (Files.exists(targetDir) && !Files.isDirectory(targetDir)) {
             argumentFail("The target directory must either not exist, or be a folder!");
         }
 
@@ -98,14 +92,6 @@ public final class CliMain implements Callable<Integer> {
         return 0;
     }
 
-    private Path getSourceDir() {
-        return srcTargetDir.get(0);
-    }
-
-    private Path getTargetDir() {
-        return srcTargetDir.get(1);
-    }
-
     private void argumentFail(String message) {
         throw new CommandLine.ParameterException(spec.commandLine(), message);
     }
@@ -113,7 +99,7 @@ public final class CliMain implements Callable<Integer> {
     private Path makeBackup() {
         try {
             BackupApi backupApi = new BackupApi(gpgRecipientId, envOverrides, maxFileSize);
-            return backupApi.makeBackup(backupName, getSourceDir(), getTargetDir());
+            return backupApi.makeBackup(backupName, sourceDir, targetDir);
         } catch (BackupTargetExistsException e) {
             logger.info("Failed to create backup: {}", e.getMessage());
             logger.debug("Failure", e);
