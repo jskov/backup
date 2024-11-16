c8e793a369530a09d43323c3b133fdc492d1069b
Jesper Skov <jskov@mada.dk>
2024-11-16 17:17:02 +0100

    closer

--- src/test/java/dk/mada/accept/NamedBackupKeepingValidArchivesTest.java
+++ src/test/java/dk/mada/accept/NamedBackupKeepingValidArchivesTest.java
@@ -7,22 +7,21 @@ import static org.junit.Assert.fail;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.Duration;
+import java.util.List;
 import java.util.Objects;
 
 import org.apache.commons.compress.archivers.ArchiveException;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import dk.mada.backup.api.BackupOutputType;
-import dk.mada.backup.impl.output.DirectoryDeleter;
-import dk.mada.backup.restore.RestoreExecutor.Result;
 import dk.mada.backup.restore.RestoreScriptReader;
+import dk.mada.backup.restore.RestoreScriptReader.DataCryptV2;
 import dk.mada.backup.restore.RestoreScriptReader.RestoreScriptData;
 import dk.mada.fixture.ExitHandlerFixture.TestFailedWithException;
 import dk.mada.fixture.MakeBackup;
-import dk.mada.fixture.MakeRestore;
 
 /**
  * Makes a backup, tweaks subset of data folders, makes another backup. Ensures that only the changed data folders are
@@ -30,6 +29,7 @@ import dk.mada.fixture.MakeRestore;
  */
 @Tag("accept")
 class NamedBackupKeepingValidArchivesTest {
+    private static final Logger logger = LoggerFactory.getLogger(NamedBackupKeepingValidArchivesTest.class);
 
     /**
      * Tests that an existing backup's BROKEN encrypted files cause an abort when trying to update.
@@ -58,35 +58,57 @@ class NamedBackupKeepingValidArchivesTest {
 
     /**
      * Tests that an existing backup's encrypted files are reused when possible.
+     *
+     * Also tests that files are added/removed/updated as needed.
      */
     @Test
-    void backupContainsInfo() throws IOException, ArchiveException, InterruptedException {
+    void updatedBackupRetainsExistingEncryptedFiles() throws IOException, ArchiveException {
         Path restoreScriptFile = MakeBackup.makeBackup(BackupOutputType.NAMED, true);
-
-        // Unpack for access to archives
-//        Path restoreDir = Paths.get("build/backup-restored");
-//        DirectoryDeleter.delete(restoreDir);
-//        Result res = MakeRestore.runRestoreCmd(restoreScriptFile, "unpack", "-a", restoreDir.toAbsolutePath().toString());
-//        System.out.println(res.output());
-
-        // TODO: change a file/folder
-
-        System.out.println("\n\n===== Making update backup ====\n\n");
-
-//        Thread.sleep(Duration.ofSeconds(61));
-        
-        Path updatedRestoreScriptFile = MakeBackup.makeBackup(BackupOutputType.NAMED, false);
 
         RestoreScriptReader reader = new RestoreScriptReader();
         RestoreScriptData originalSet = reader.readRestoreScriptData(restoreScriptFile);
-        RestoreScriptData updatedSet = reader.readRestoreScriptData(updatedRestoreScriptFile);
-        
-        assertThat(originalSet.cryptsV2())
-                .isEqualTo(updatedSet.cryptsV2());
-    }
 
-    // TODO: with added folder/file
-    // TODO: with removed folder/file
+        // Now make an updated backup, but change some of the src files
+        Path updatedRestoreScriptFile = MakeBackup.makeBackup(BackupOutputType.NAMED, false, srcDir -> {
+            Path extraFile = srcDir.resolve("extra-file");
+            Files.writeString(extraFile, "extra file text");
+
+            Path extraDir = srcDir.resolve("extra-dir");
+            Files.createDirectories(extraDir);
+            Files.createFile(extraDir.resolve("dummy-file"));
+
+            Path existingDir = srcDir.resolve("dir-a");
+            Files.createFile(existingDir.resolve("new-a-file"));
+
+            Path existingFile = srcDir.resolve("file-tricky.tar");
+            Files.delete(existingFile);
+
+            logger.info("XXX Modified src directory {}", srcDir);
+        });
+
+        RestoreScriptData updatedSet = reader.readRestoreScriptData(updatedRestoreScriptFile);
+
+        logger.info("Original: {}", originalSet.cryptsV2());
+        logger.info("Updated: {}", updatedSet.cryptsV2());
+
+        List<DataCryptV2> sharedCrypts = originalSet.cryptsV2().stream()
+                .filter(updatedSet.cryptsV2()::contains)
+                .toList();
+        List<String> cryptNamesOnlyInOriginalSet = originalSet.cryptsV2().stream()
+                .filter(c -> !sharedCrypts.contains(c))
+                .map(c -> c.file().getFileName().toString())
+                .toList();
+        List<String> cryptNamesOnlyInUpdatedSet = updatedSet.cryptsV2().stream()
+                .filter(c -> !sharedCrypts.contains(c))
+                .map(c -> c.file().getFileName().toString())
+                .toList();
+
+        logger.info("Matching crypts: {}", sharedCrypts);
+        assertThat(cryptNamesOnlyInOriginalSet)
+            .containsExactlyInAnyOrder("dir-a.crypt", "file-tricky.tar.crypt");
+        assertThat(cryptNamesOnlyInUpdatedSet)
+            .containsExactlyInAnyOrder("dir-a.crypt", "extra-dir.crypt", "extra-file.crypt");
+    }
 
     private Path parentDir(Path restoreScript) {
         return Objects.requireNonNull(restoreScript.getParent(), "No parent for restore script?!");
