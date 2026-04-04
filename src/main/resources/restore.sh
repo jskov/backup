78f6ab26af949d3cd98dd177a242cd291b78c40a
Jesper Skov <jskov@mada.dk>
2026-04-04 17:54:20 +0200

    Handle different output from XXH3 on Fedora and Ubuntu (#272)

--- src/main/resources/restore.sh
+++ src/main/resources/restore.sh
@@ -50,7 +50,16 @@ expect_file() {
     fi
 
     local xxh3_output=$(/bin/xxhsum -H3 "$file")
-    local actual_xxh3=${xxh3_output: -16}
+    # This output changed between versions, needing separate decoding for Fedora and Ubuntu
+    if [[ -f /etc/redhat-release ]]; then
+        # Newer versions of xxh3 prefix with XXH3_hex16
+        # XXH3_2d06800538d394c2  /tmp/a
+        local actual_xxh3=${xxh3_output:5:16}
+    else
+        # Older versions of xxh3 (as used in Ubuntu) outputs checksum at the end
+        # XXH3 (/tmp/a) = 2d06800538d394c2
+        local actual_xxh3=${xxh3_output: -16}
+    fi
     if [[ "$actual_xxh3" != "$xxh3" ]]; then
         fail "\nFile $file has xxh3 '$actual_xxh3', but expected '$xxh3'"
     fi
@@ -297,7 +306,11 @@ set -e
 
 filename="\$1"
 
-a=\$(/bin/xxhsum -H3 - | echo "\$(/bin/cut -d' ' -f4),\$filename")
+if [[ -f /etc/redhat-release ]]; then
+  a=\$(/bin/xxhsum -H3 - | echo "\$(/bin/cut -c 6-21),\$filename")
+else
+  a=\$(/bin/xxhsum -H3 - | echo "\$(/bin/cut -d' ' -f4),\$filename")
+fi
 
 if ! (/bin/grep -F -q "\$a" /tmp/valid-input.txt) ; then
   echo >/dev/stderr "Did not find matching checksum for file '\$filename'"
