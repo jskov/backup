42ddabc551195f60266b4c9099ba4ae68563930f
Jesper Skov <jskov@mada.dk>
2026-04-06 12:44:47 +0200

    Rework LoggerCapture constructor to avoid breaking checkstyle

--- src/test/java/dk/mada/fixture/LoggerCapture.java
+++ src/test/java/dk/mada/fixture/LoggerCapture.java
@@ -11,27 +11,31 @@ import org.jspecify.annotations.Nullable;
 /**
  * Logger output capture for testing.
  */
-public class LoggerCapture extends StreamHandler {
+public final class LoggerCapture extends StreamHandler {
     /** The last created instance. Normally just one instance is created. */
     @Nullable private static LoggerCapture instance;
     /** The consumed output. */
     private final ByteArrayOutputStream bos;
 
     /**
-     * Create a {@code ConsoleHandler} for {@code System.err}.
-     * <p>
-     * The {@code ConsoleHandler} is configured based on {@code LogManager}
-     * properties (or their default values).
-     *
+     * Creates instance.
      */
     public LoggerCapture() {
-        var bos = new ByteArrayOutputStream();
+        this(new ByteArrayOutputStream());
+    }
+
+    /**
+     * Creates instance.
+     *
+     * @param bos the capturing output stream
+     */
+    private LoggerCapture(ByteArrayOutputStream bos) {
         super(bos, new SimpleFormatter());
 
         this.bos = bos;
         LoggerCapture.instance = this;
 
-        System.err.println("WARNING: Created LoggerCapture instance " + this);
+        System.err.println("WARNING: Created LoggerCapture instance " + this.hashCode());
     }
 
     @Override
